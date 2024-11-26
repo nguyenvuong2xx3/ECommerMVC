e624a503d0c356f1cd0991fc8d7bcdbe4c0df188
nguyenvuong2xx3 <2121051239@student.humg.edu.vn>
2024-11-26 23:38:26 +0700

    add ECommerMVC ...26/11

---
+++ ECommerMVC/ViewComponents/MenuLoaiViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using ECommerMVC.Data;
+using ECommerMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerMVC.ViewComponents
+{
+    public class MenuLoaiViewComponent : ViewComponent
+    {
+        private readonly Hshop2023Context db;
+
+        public MenuLoaiViewComponent(Hshop2023Context context) => db = context;
+
+        public IViewComponentResult Invoke()
+        {
+            var data = db.Loais.Select(lo => new MenuLoaiVM
+            {
+                MaLoai = lo.MaLoai,
+                TenLoai = lo.TenLoai,
+                SoLuong = lo.HangHoas.Count
+            });
+            return View(data);// Default.cshtml
+            //return View(data);
+        }
+    }
+
+}
